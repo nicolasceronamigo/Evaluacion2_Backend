04c69c5c79d4acc865b7f690d17150fbc0fc9eec
nicolasceronamigo <nicolasceronamigo@gmail.com>
2026-09-22 12:45:14 -0300

    Se crea el proyecto con una aplicacion y con el modelo Libro.

---
+++ librosapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Libro(models.Model):
+    ESTADOS = [
+        ("D", "Disponible"),
+        ("P", "Prestado"),
+        ("R", "Reservado"),
+        ("DA", "Dañado"),
+        ("PE", "Perdido"),
+    ]
+    codigo = models.CharField(max_length=20, unique=True)
+    titulo = models.CharField(max_length=100)
+    autor=models.CharField(max_length=100)
+    editorial=models.CharField(max_length=50)
+    estado = models.CharField(
+        max_length=2,
+        choices=ESTADOS,
+        default="D"
+    )
